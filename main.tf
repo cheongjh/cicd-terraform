d86ff5f526ff90d6e65897639d92db6bd5608eae
unknown <cheongjh@gmail.com>
2024-01-25 01:40:01 +0800

    first commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce5-tfstate-bucket-1"
+    key    = "leon.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_sns_topic" "user_updates" {
+  name = "leon-updates-topic"
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+This is my terraform version 
+Terraform v1.6.6
+on windows_amd64
